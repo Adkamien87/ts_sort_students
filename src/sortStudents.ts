21303a1b4ea54632234151d594b0c9a15c00b1da
Adam <ad.kamieniarz@gmail.com>
2025-07-18 12:12:33 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,96 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrades'
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function getAverageGrade(grades: number[]): number {
+  return grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student {
+  const sortedStudents: Student[] = [...students];
+  let result: Student[];
+
+  switch (sortBy) {
+    case SortType.Age:
+      if (order === 'asc') {
+        result = sortedStudents.sort(
+          (user1, user2) => user1.age - user2.age,
+        );
+      }
+
+      if (order === 'desc') {
+        result = sortedStudents.sort(
+          (user1, user2) => user2.age - user1.age,
+        );
+      }
+
+      break;
+
+    case SortType.Name:
+    case SortType.Surname:
+      if (order === 'asc') {
+        result = sortedStudents.sort(
+          (user1, user2) => user1[sortBy].localeCompare(user2[sortBy]),
+        );
+      }
+
+      if (order === 'desc') {
+        result = sortedStudents.sort(
+          (user1, user2) => user2[sortBy].localeCompare(user1[sortBy]),
+        );
+      }
+      break;
+
+    case SortType.Married:
+      if (order === 'asc') {
+        result = sortedStudents.sort(
+          (user1, user2) => Number(user1.married) - Number(user2.married),
+        );
+      }
+
+      if (order === 'desc') {
+        result = sortedStudents.sort(
+          (user1, user2) => Number(user2.married) - Number(user1.married),
+        );
+      }
+
+      break;
+
+    case SortType.AverageGrade:
+      if (order === 'desc') {
+        result = students.sort((student1, student2) => getAverageGrade(
+          student2.grades,
+        ) - getAverageGrade(student1.grades));
+      }
+
+      if (order === 'asc') {
+        result = students.sort((student1, student2) => getAverageGrade(
+          student1.grades,
+        ) - getAverageGrade(student2.grades));
+      }
+
+      break;
+
+    default: throw new Error('Type is not valid');
+  }
+
+  return result;
 }
